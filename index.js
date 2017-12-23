f2c502cbae5d88cdd79b35cd691180e06bc75a5e
Vitao18 <victoronline9@gmail.com>
2017-12-23 21:16:37 -0200

    added google auth keys

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+
+passport.use(new GoogleStrategy());
 
 app.listen(PORT);
 
