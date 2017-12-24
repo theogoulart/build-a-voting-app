53ef392b6613cf228c6d45963330c56b7305f668
Vitao18 <victoronline9@gmail.com>
2017-12-23 23:12:23 -0200

    added nodemon

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
+
+const keys = require('./config/keys');
 const PORT = process.env.PORT || 5000;
 
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback', 
+}, acessToken  => console.log('AcessToken is', acessToken)));
+
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email'],
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 
 app.listen(PORT);
 
