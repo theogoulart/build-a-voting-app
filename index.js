468050802d53a542a893e2fbb09105cee3cd7a37
Vitao18 <victoronline9@gmail.com>
2017-12-25 13:16:07 -0200

    added users model

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 
 require('./routes/authRoutes')(app);
 require('./services/passport');
+require('./models/User');
+
+mongoose.connect(require('./config/keys').mongoURI);
 
 const PORT = process.env.PORT || 5000;
 
