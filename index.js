8dd6b6a08156fb36f6607d005fec1fdb49f9ac69
Vitao18 <victoronline9@gmail.com>
2017-12-24 22:01:57 -0200

    refactor of app and added mongoose

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const app = express();
 
+require('./routes/authRoutes')(app);
+require('./services/passport');
 
-const keys = require('./config/keys');
 const PORT = process.env.PORT || 5000;
-
-
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback', 
-}, acessToken  => console.log('AcessToken is', acessToken)));
-
-
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email'],
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
 
 app.listen(PORT);
 
 module.exports = app;
-
