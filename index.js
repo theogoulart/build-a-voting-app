98558f6bf33752668e9e75fe0c6582e0643911aa
Vitao18 <victoronline9@gmail.com>
2017-12-25 20:52:24 -0200

    added cookie

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+
 const app = express();
 
-require('./routes/authRoutes')(app);
-require('./services/passport');
 require('./models/User');
+require('./services/passport');
 
-mongoose.connect(require('./config/keys').mongoURI);
+mongoose.connect(keys.mongoURI);
+
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [keys.cookieKey],
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,4 +8,10 @@ module.exports = app => {
   }));
   
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/logout', (req, res) => req.logout());
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(`Current user is ${req.user}`);
+  });
 }
