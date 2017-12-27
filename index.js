feed28d64cab59ad61e3287952d5a312103dd261
Vitao18 <victoronline9@gmail.com>
2017-12-27 12:02:02 -0200

    added body-parser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 
 const app = express();
@@ -16,6 +17,7 @@ app.use(cookieSession({
   maxAge: 30 * 24 * 60 * 60 * 1000,
   keys: [keys.cookieKey],
 }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
 app.use(passport.session());
--- routes/pollRoutes.js
+++ routes/pollRoutes.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const requireLogin = require('../middlewares/requireLogin');
+
+const Poll = mongoose.model('polls');
+
 const app = express();
 
 module.exports = app => {
   app.get('/api/polls', (req, res) => {
-    return 'pollssss';
+    res.send('pollssss');
   });
 
   app.get('/api/polls/:id', (req, res) => {
-    return "thisss poll"
+    res.send('pollssss');
   });
 
-  app.post('api/polls', requireLogin, (req, res) => {
-    res.send("BORA, HORA DO SHOW PORRA!");
+  app.post('/api/polls', requireLogin, (req, res) => {
+    const { title, options } = req.body;
+    new Poll({ title, options, _user: req.user }).save();
+    res.redirect('/api/polls');
   });
 }
