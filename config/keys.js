4a39fda298f288c4625577554616f211b8f5a208
Vitao18 <victoronline9@gmail.com>
2018-01-27 20:41:12 -0200

    refactored api calls to keys

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,4 @@
-const production = require('./prod');
-const dev = require('./dev');
-
 const env = process.env.NODE_ENV;
 
-const config = {
-  dev,
-  production,
-};
-
-module.exports = config[env] || config.dev;
+module.exports = env === 'production' ?
+  require('./prod') : require('./dev');
